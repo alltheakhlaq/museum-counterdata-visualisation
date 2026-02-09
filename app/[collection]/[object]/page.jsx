64bc2e610a5d8b521094479b96cb6c5e5c96e2ad
Sara <77059785+alltheakhlaq@users.noreply.github.com>
2026-02-09 11:18:21 +0000

    object page update

--- app/[collection]/[object]/page.jsx
+++ app/[collection]/[object]/page.jsx
@@ -27,7 +27,93 @@ export default async function ObjectPage({ params }) {
 
   return (
     <div>
-      Object page for object "{object}" in {collection} collection
+      {/* Object page for object "{object}" in {collection} collection */}
+      <div className="link">
+        <div>Back to collection</div>
+      </div>
+      <div className="box">
+        <h2>Object title:</h2>
+        <p>Name of one of the objects from the Sloane Collection</p>
+      </div>
+      <div className="flex flex-row w-full gap-5">
+        <div className="flex-1">
+          <div className="square"></div>
+        </div>
+        <div>
+          <div className="flex flex-col cursor-pointer gap-6">
+            <div className="flex">
+              <div className="rounded-full border border-black-20 bg-[#2eb5a5] size-10 m-auto"></div>
+            </div>
+            <div className="flex">
+              <div className=" rounded-full border border-black-20 bg-[#ffcf56] size-10 m-auto"></div>
+            </div>
+            <div className="flex">
+              <div className=" rounded-full border border-black-20 bg-[#d36c83] size-10 m-auto"></div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-3 mr-8 max-h-[700px] overflow-y-scroll ">
+          <section className="odibox">
+            <div id="label1">
+              <h2>Data Interventions</h2>{" "}
+            </div>
+            {/* <dl>
+              {Object.entries(collectionItems[objectIndex].dataFields).map(
+                ([fieldName, fieldInfo]) => {
+                  const fieldValues = Array.isArray(fieldInfo)
+                    ? fieldInfo
+                    : [fieldInfo];
+                  return (
+                    <>
+                      <dt>
+                        <strong>{fieldName}</strong>
+                      </dt>
+                      {fieldValues.map(({ fieldValue, contextTag }) => (
+                        <dd
+                          className="mb-4"
+                          onClick={() => {
+                            const context = document.querySelector(
+                              `[data-context-tag="${contextTag}"]`,
+                            );
+                            context?.scrollIntoView({ behavior: "smooth" });
+                          }}
+                          style={contextTag ? { cursor: "pointer" } : undefined}
+                        >
+                          {fieldValue}
+                        </dd>
+                      ))}
+                    </>
+                  );
+                },
+              )}
+            </dl> */}
+          </section>
+          <section className="pdbox">
+            <div id="label2">
+              <h2>Power Dimensions</h2>
+            </div>
+            <div className="font-bold">Historical Power Dimensions</div>
+            <div>
+              Forms of domination that could be inspected through the object:
+            </div>
+            <div>
+              Forms of resistance that could be inspected through the object:
+            </div>
+            <br />
+            <div className="font-bold">Contemporary Power Dimensions</div>
+            <div>Structural Domain</div>
+            <div>Disciplinary Domain</div>
+            <div>Hegemonic Domain</div>
+            <div>Interpersonal Domain</div>
+          </section>
+          <section className="cabox">
+            <div id="label3">
+              <h2>Accessibility Information</h2>
+            </div>
+            <div>counterdata contents 3</div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
--- app/[collection]/page.jsx
+++ app/[collection]/page.jsx
@@ -65,20 +65,20 @@ export default async function CollectionPage({ params }) {
         {infoBox}
         <label>
           <input className="peer/showLabel absolute scale-0" type="checkbox" />
-          <span className="block max-h-15 w-full overflow-hidden bg-[#ffffe0] rounded-2xl border border-black border-2 p-3 duration-300 peer-checked/showLabel:max-h-52">
+          <span className="block max-h-15 w-full overflow-hidden bg-[#ffffe0] rounded-2xl border border-black border-2 p-3 duration-300 peer-checked/showLabel:max-h-1000">
             <div className="flex flex-row gap-10 justify-center">
               <p className="flex cursor-pointer items-center">
                 Explore object in Sloane's collection and their encompassing
                 power dimensions{" "}
               </p>
               <div
-                className="flex mt-2 w-0 h-0 
-                  border-l-[10px] border-l-transparent
+                className="flex mt-2 w-0 h-0 {
+          }        border-l-[10px] border-l-transparent
                   border-t-[15px] border-t-black-500
                   border-r-[10px] border-r-transparent"
               ></div>
             </div>
-            <div className="grid grid-cols-3 mt-10">
+            <div className="grid grid-cols-2 w-full mt-10 gap-5">
               <div>
                 <a href={`${collection}/object-1`}>Object 1</a>
               </div>
